a9fcb30c5c0dead3d362cb25d6650871b1b72f7e
Angus Kellett <anguskellett@hotmail.com>
2021-01-27 18:38:06 +0000

    imports composers component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComposersComponent } from './composers/composers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComposersComponent
   ],
   imports: [
     BrowserModule,
