6b27aae6e6819c7ae64e81b53683c8d8fed62613
Angus Kellett <anguskellett@hotmail.com>
2021-01-27 18:38:52 +0000

    initial commit

---
+++ src/app/composers/composers.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ComposersComponent } from './composers.component';
+
+describe('ComposersComponent', () => {
+  let component: ComposersComponent;
+  let fixture: ComponentFixture<ComposersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ComposersComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ComposersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/composers/composers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-composers',
+  templateUrl: './composers.component.html',
+  styleUrls: ['./composers.component.css']
+})
+export class ComposersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
