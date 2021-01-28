702955ad3719723d84d32ed2f480a35570209e5d
Angus Kellett <anguskellett@hotmail.com>
2021-01-28 07:40:01 +0000

    imports operas component and opera item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComposersComponent } from './composers/composers.component';
+import { OperasComponent } from './operas/operas.component';
+import { OperaItemComponent } from './opera-item/opera-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComposersComponent
+    ComposersComponent,
+    OperasComponent,
+    OperaItemComponent
   ],
   imports: [
     BrowserModule,
