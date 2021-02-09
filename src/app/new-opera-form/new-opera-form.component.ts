0d00092da988d1439468a1219a9692f16baaf16a
Angus Kellett <anguskellett@hotmail.com>
2021-02-09 23:36:16 +0000

    initial commit

---
+++ src/app/new-opera-form/new-opera-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-opera-form',
+  templateUrl: './new-opera-form.component.html',
+  styleUrls: ['./new-opera-form.component.css']
+})
+export class NewOperaFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
