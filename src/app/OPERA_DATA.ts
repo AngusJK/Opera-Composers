4138af117dd4da346421f5d2ed3993f9760f4904
Angus Kellett <anguskellett@hotmail.com>
2021-02-09 23:36:48 +0000

    houses the opera data

---
+++ src/app/OPERA_DATA.ts
@@ -0,0 +1,21 @@
+export const OPERA_DATA = [{
+  id: 1,
+  name: "Don Giovanni",
+  composer: "Mozart",
+  language: "Italian",
+  year: 1789
+},
+{
+  id: 2,
+  name: "Götterdämmerung",
+  composer: "Wagner",
+  language: "German",
+  year: 1876
+},
+{
+  id: 3,
+  name: "La bohème",
+  composer: "Puccini",
+  language: "Italian",
+  year: 1896
+}]
