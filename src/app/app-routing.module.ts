350cdf902ca09f587e5db327f100f4c3f785daeb
Angus Kellett <anguskellett@hotmail.com>
2021-03-11 01:49:23 +0000

    adds paths to operas, composers, and operas/:id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComposersComponent } from './composers/composers.component';
+import { OperaItemComponent } from './opera-item/opera-item.component';
+import { OperasComponent } from './operas/operas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'operas', component: OperasComponent },
+  { path: 'composers', component: ComposersComponent },
+  { path: 'operas/:id', component: OperaItemComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
