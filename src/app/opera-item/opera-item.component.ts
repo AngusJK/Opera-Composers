d50d753bc038f15ccb3c26645ce35113fb950f28
Angus Kellett <anguskellett@hotmail.com>
2021-03-12 04:04:09 +0000

    imports activatedRoute

--- src/app/opera-item/opera-item.component.ts
+++ src/app/opera-item/opera-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Opera } from 'src/app/models/Opera';
+import { OPERA_DATA } from '../OPERA_DATA';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-opera-item',
@@ -11,10 +13,16 @@ export class OperaItemComponent implements OnInit {
    * creates input property
    */
   @Input() opera!: Opera;
+  operas = OPERA_DATA;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap
+    .subscribe(params => {
+
+
+    })
   }
 
 }
