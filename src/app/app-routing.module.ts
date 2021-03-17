2cd189d0abb131bffccf88b9711ab251929e689d
Angus Kellett <anguskellett@hotmail.com>
2021-03-17 22:49:15 +0000

    adds route to new opera form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { ComposersComponent } from './composers/composers.component';
 import { LoginComponent } from './login/login.component';
 import { OperaItemComponent } from './opera-item/opera-item.component';
 import { OperasComponent } from './operas/operas.component';
+import { NewOperaFormComponent } from './new-opera-form/new-opera-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'operas', pathMatch: 'full' },
   { path: 'operas', component: OperasComponent },
   { path: 'composers', component: ComposersComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'operas/:id', component: OperaItemComponent }
+  { path: 'operas/:id', component: OperaItemComponent },
+  { path: 'new', component: NewOperaFormComponent },
+
 ];
 
 @NgModule({
