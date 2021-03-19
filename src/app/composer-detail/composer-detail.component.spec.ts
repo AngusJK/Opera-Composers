6db3e4438b6010b2a6344b7b7a128471e56f7af4
Angus Kellett <anguskellett@hotmail.com>
2021-03-19 00:15:59 +0000

    initial commit

---
+++ src/app/composer-detail/composer-detail.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ComposerDetailComponent } from './composer-detail.component';
+
+describe('ComposerDetailComponent', () => {
+  let component: ComposerDetailComponent;
+  let fixture: ComponentFixture<ComposerDetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ComposerDetailComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ComposerDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/composer-detail/composer-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-composer-detail',
+  templateUrl: './composer-detail.component.html',
+  styleUrls: ['./composer-detail.component.css']
+})
+export class ComposerDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
