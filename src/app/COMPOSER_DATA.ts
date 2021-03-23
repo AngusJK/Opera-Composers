8770abfed6b1bc25a8e0eb87f3b0ace691c260e0
Angus Kellett <anguskellett@hotmail.com>
2021-03-23 17:19:35 +0000

    contains composer data

---
+++ src/app/COMPOSER_DATA.ts
@@ -0,0 +1,48 @@
+export const COMPOSER_DATA = [
+
+
+    { id: 1,
+      firstName: "Wolfgang Amadeus",
+      lastName: "Mozart",
+      birthYear: 1756,
+      deathYear: 1791,
+      country: "Austria" },
+    { id: 2,
+      firstName: "Richard",
+      lastName: "Wagner",
+      birthYear: 1813,
+      deathYear: 1886,
+      country: "Germany" },
+    { id: 3,
+      firstName: "Giuseppe",
+      lastName: "Verdi",
+      birthYear: 1813,
+      deathYear: 1901,
+      country: "Italy" },
+    { id: 4,
+      firstName: "Giacomo",
+      lastName: "Puccini",
+      birthYear: 1856,
+      deathYear: 1924,
+      country: "Italy" },
+    { id: 5, firstName: "Benjamin",
+      lastName: "Britten",
+      birthYear: 1913,
+      deathYear: 1976,
+      country: "Great Britain" },
+    { id: 6,
+      firstName: "Richard",
+      lastName: "Strauss",
+      birthYear: 1864,
+      deathYear: 1949,
+      country: "Germany" },
+    { id: 7,
+      firstName: "Sergei",
+      lastName: "Prokofiev",
+      birthYear: 1891,
+      deathYear: 1953,
+      country: "Russia" }
+
+
+
+]
