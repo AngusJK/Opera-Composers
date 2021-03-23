1206a62328752ebca558355aab12062c93873ccb
Angus Kellett <anguskellett@hotmail.com>
2021-03-23 17:20:50 +0000

    imports OPERA_DATA

---
+++ src/app/opera-item.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { of, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Opera } from './models/Opera';
+import { OPERA_DATA } from './OPERA_DATA';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OperaItemService {
+
+  operas: any;
+
+  constructor(private http: HttpClient) {
+    this.operas = OPERA_DATA;
+  }
+
+  getOpera(id:number) : Observable<Opera> {
+    return this.getOperaList()
+    .pipe(
+      map(operas => {
+        return operas.filter(opera => opera.id === id)[0]
+      })
+    )
+  }
+
+  getOperaList() : Observable<Opera[]> {
+    return of( this.operas )
+  }
+}
