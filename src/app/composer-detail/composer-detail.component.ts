8f39b768bea26017431b2456775de7b2b2bfe6fe
Angus Kellett <anguskellett@hotmail.com>
2021-03-23 04:58:27 +0000

    hardcodes composers array

--- src/app/composer-detail/composer-detail.component.ts
+++ src/app/composer-detail/composer-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-composer-detail',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComposerDetailComponent implements OnInit {
 
-  constructor() { }
+  composers =  [
+    { id: 1,
+      firstName: "Wolfgang Amadeus",
+      lastName: "Mozart",
+      birthYear: 1756,
+      deathYear: 1791,
+      country: "Austria" },
+    { id: 2,
+      firstName: "Richard",
+      lastName: "Wagner",
+      birthYear: 1813,
+      deathYear: 1886,
+      country: "Germany" },
+    { id: 3,
+      firstName: "Giuseppe",
+      lastName: "Verdi",
+      birthYear: 1813,
+      deathYear: 1901,
+      country: "Italy" },
+    { id: 4,
+      firstName: "Giacomo",
+      lastName: "Puccini",
+      birthYear: 1856,
+      deathYear: 1924,
+      country: "Italy" },
+    { id: 5, firstName: "Benjamin",
+      lastName: "Britten",
+      birthYear: 1913,
+      deathYear: 1976,
+      country: "Great Britain" },
+    { id: 6,
+      firstName: "Richard",
+      lastName: "Strauss",
+      birthYear: 1864,
+      deathYear: 1949,
+      country: "Germany" },
+    { id: 7,
+      firstName: "Sergei",
+      lastName: "Prokofiev",
+      birthYear: 1891,
+      deathYear: 1953,
+      country: "Russia" }
+  ];
+
+
+  constructor(private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+    this.route.paramMap
+    .subscribe(params => {
+      console.log(params);
+    })
   }
+
+
 
 }
