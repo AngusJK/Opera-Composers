af64b3b007c1b40cb36a7033d64e5b956b6e53c8
Angus Kellett <anguskellett@hotmail.com>
2021-03-24 23:38:18 +0000

    adds three new operas to database

--- src/app/OPERA_DATA.ts
+++ src/app/OPERA_DATA.ts
@@ -1,21 +1,44 @@
-export const OPERA_DATA = [{
-  id: 1,
-  name: "Don Giovanni",
-  composer: "Mozart",
-  language: "Italian",
-  year: 1789
-},
-{
-  id: 2,
-  name: "Götterdämmerung",
-  composer: "Wagner",
-  language: "German",
-  year: 1876
-},
-{
-  id: 3,
-  name: "La bohème",
-  composer: "Puccini",
-  language: "Italian",
-  year: 1896
-}]
+export const OPERA_DATA = [
+  {
+    id: 1,
+    name: "Don Giovanni",
+    composer: "Mozart",
+    language: "Italian",
+    year: 1789
+  },
+  {
+    id: 2,
+    name: "Götterdämmerung",
+    composer: "Wagner",
+    language: "German",
+    year: 1876
+  },
+  {
+    id: 3,
+    name: "La bohème",
+    composer: "Puccini",
+    language: "Italian",
+    year: 1896
+  },
+  {
+    id: 4,
+    name: "Tosca",
+    composer: "Puccini",
+    language: "Italian",
+    year: 1900
+  },
+  {
+    id: 5,
+    name: "Elektra",
+    composer: "Strauss",
+    language: "German",
+    year: 1909
+  },
+  {
+    id: 6,
+    name: "La traviata",
+    composer: "Verdi",
+    language: "Italian",
+    year: 1856
+  }
+]
