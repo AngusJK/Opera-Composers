d23d1c951d6888e358218e58f130a175743258da
Angus Kellett <anguskellett@hotmail.com>
2021-03-25 20:54:52 +0000

    adds at least three operas for each composer

--- src/app/OPERA_DATA.ts
+++ src/app/OPERA_DATA.ts
@@ -39,6 +39,125 @@ export const OPERA_DATA = [
     name: "La traviata",
     composer: "Verdi",
     language: "Italian",
-    year: 1856
+    year: 1853
+  },
+  {
+    id: 7,
+    name: "Rigoletto",
+    composer: "Verdi",
+    language: "Italian",
+    year: 1851
+  },
+  {
+    id: 8,
+    name: "Aida",
+    composer: "Verdi",
+    language: "Italian",
+    year: 1871
+  },
+  {
+    id: 9,
+    name: "Otello",
+    composer: "Verdi",
+    language: "Italian",
+    year: 1887
+  },
+  {
+    id: 10,
+    name: "Le nozze di Figaro",
+    composer: "Mozart",
+    language: "Italian",
+    year: 1871
+  },
+  {
+    id: 11,
+    name: "Die Zauberflöte",
+    composer: "Mozart",
+    language: "German",
+    year: 1871
+  },
+  {
+    id: 12,
+    name: "Tristan und Isolde",
+    composer: "Wagner",
+    language: "German",
+    year: 1871
+  },
+  {
+    id: 13,
+    name: "Parsifal",
+    composer: "Wagner",
+    language: "German",
+    year: 1871
+  },
+  {
+    id: 14,
+    name: "Lohengrin",
+    composer: "Wagner",
+    language: "German",
+    year: 1871
+  },
+  {
+    id: 15,
+    name: "Madama Butterfly",
+    composer: "Puccini",
+    language: "Italian",
+    year: 1904
+  },
+  {
+    id: 16,
+    name: "Peter Grimes",
+    composer: "Britten",
+    language: "English",
+    year: 1939
+  },
+  {
+    id: 17,
+    name: "A Midsummer Night's Dream",
+    composer: "Britten",
+    language: "English",
+    year: 1943
+  },
+  {
+    id: 18,
+    name: "The Rape of Lucretia",
+    composer: "Britten",
+    language: "English",
+    year: 1840
+  },
+  {
+    id: 19,
+    name: "Der Rosenkavalier",
+    composer: "Strauss",
+    language: "German",
+    year: 1911
+  },
+  {
+    id: 20,
+    name: "Ariadne auf Naxos",
+    composer: "Strauss",
+    language: "German",
+    year: 1915
+  },
+  {
+    id: 21,
+    name: "War and Peace",
+    composer: "Prokofiev",
+    language: "Russian",
+    year: 1933
+  },
+  {
+    id: 22,
+    name: "The Love for Three Oranges",
+    composer: "Prokofiev",
+    language: "Russian",
+    year: 1841
+  },
+  {
+    id: 23,
+    name: "The Fiery Angel",
+    composer: "Prokofiev",
+    language: "Russian",
+    year: 1845
   }
 ]
