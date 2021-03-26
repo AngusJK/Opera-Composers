5cb272f8ddb2642f83395d09970558979d7005b4
Angus Kellett <anguskellett@hotmail.com>
2021-03-26 17:38:54 +0000

    adds synopses for three puccini operas

--- src/app/OPERA_DATA.ts
+++ src/app/OPERA_DATA.ts
@@ -18,14 +18,16 @@ export const OPERA_DATA = [
     name: "La bohème",
     composer: "Puccini",
     language: "Italian",
-    year: 1896
+    year: 1896,
+    summary: "La Bohème is one of the three operas - the others are Carmen and Aida - believed to be the most popular ever written. In the case of La Bohème the reason is that it virtually defines the term 'romantic'. The poignant story of Mimì and Rodolfo is told in music of such tender beauty, allied, as always, to Puccini's intuition of what works in the theatre, resulting in an opera that readily appeals to our emotions and senses. 'Che gelida manina - Your tiny hand is frozen...' The moment when Mimì and Rodolfo meet and first touch is one of the very great moments of opera - created by the master hand of Giacomo Puccini, who unerringly trod the fine line between romance and sentimentality. Thomson Smillie outlines the background to the opera, explaining how painstakingly the composer worked to achieve this effect. An absorbing introduction both for those approaching La Bohème for the first time and for those wanting to know a bit more than just the big arias."
   },
   {
     id: 4,
     name: "Tosca",
     composer: "Puccini",
     language: "Italian",
-    year: 1900
+    year: 1900,
+    summary: "Tosca is Puccini at the peak of his theatrical power. The story of the jealous, impassioned opera singer Floria Tosca and her doomed love for the painter Mario Cavaradossi is played out against backgrounds both historically and geographically overwhelming. It is set in three great and historical locations of Rome during the Napoleonic era. Spectacle, sensuality and cruelty battle for our attention in one of the most truly 'action-packed' works of theatre. Enticing us with just a couple of the 'great tunes' from this deeply affecting opera, David Timson then begins setting the biographical and operatic scene. The enduring popularity of Tosca is quickly understood - its appeal generated largely from exactly the same elements which once caused academics to brand it a 'shabby little shocker'. The master of melody is at his finest, as David Timson illustrates with characteristic enthusiasm."
   },
   {
     id: 5,
@@ -102,7 +104,8 @@ export const OPERA_DATA = [
     name: "Madama Butterfly",
     composer: "Puccini",
     language: "Italian",
-    year: 1904
+    year: 1904,
+    summary: "Madama Butterfly runs the gamut of operatic emotions. The title heroine has a fragility wonderfully expressed in her entry music, deep sexual instincts as shown in the love duet, and immense tragic stature in her last hours. The fate of Madama Butterfly - married to an American sailor and then abandoned - engages our emotions, as it obviously did Puccini's to a degree equalled by few other opera heroines. The oriental setting and the exquisite orchestral and choral writings are treasured bonuses. This audiobook is the perfect introduction to Madama Butterfly. We learn about the background to Puccini's 'Japanese' opera, the social themes of honour and loyalty it involves, and how it progressed from a surprisingly unsuccessful premiere to one of the central and best-loved works of the international operatic repertory. As the story unfolds, we get to know the melodies which make this such a moving and compelling opera."
   },
   {
     id: 16,
