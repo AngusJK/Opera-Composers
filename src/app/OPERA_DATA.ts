d2f982a7c2da2e76cbdd661d638032739ab47271
Angus Kellett <anguskellett@hotmail.com>
2021-03-28 17:26:23 +0000

    adds first names to composers

--- src/app/OPERA_DATA.ts
+++ src/app/OPERA_DATA.ts
@@ -2,21 +2,21 @@ export const OPERA_DATA = [
   {
     id: 1,
     name: "Don Giovanni",
-    composer: "Mozart",
+    composer: "Wolfgang Amadeus Mozart",
     language: "Italian",
     year: 1789
   },
   {
     id: 2,
     name: "Götterdämmerung",
-    composer: "Wagner",
+    composer: "Richard Wagner",
     language: "German",
     year: 1876
   },
   {
     id: 3,
     name: "La bohème",
-    composer: "Puccini",
+    composer: "Giacomo Puccini",
     language: "Italian",
     year: 1896,
     summary: "La Bohème is one of the three operas - the others are Carmen and Aida - believed to be the most popular ever written. In the case of La Bohème the reason is that it virtually defines the term 'romantic'. The poignant story of Mimì and Rodolfo is told in music of such tender beauty, allied, as always, to Puccini's intuition of what works in the theatre, resulting in an opera that readily appeals to our emotions and senses. 'Che gelida manina - Your tiny hand is frozen...' The moment when Mimì and Rodolfo meet and first touch is one of the very great moments of opera - created by the master hand of Giacomo Puccini, who unerringly trod the fine line between romance and sentimentality. Thomson Smillie outlines the background to the opera, explaining how painstakingly the composer worked to achieve this effect. An absorbing introduction both for those approaching La Bohème for the first time and for those wanting to know a bit more than just the big arias."
@@ -24,7 +24,7 @@ export const OPERA_DATA = [
   {
     id: 4,
     name: "Tosca",
-    composer: "Puccini",
+    composer: "Giacomo Puccini",
     language: "Italian",
     year: 1900,
     summary: "Tosca is Puccini at the peak of his theatrical power. The story of the jealous, impassioned opera singer Floria Tosca and her doomed love for the painter Mario Cavaradossi is played out against backgrounds both historically and geographically overwhelming. It is set in three great and historical locations of Rome during the Napoleonic era. Spectacle, sensuality and cruelty battle for our attention in one of the most truly 'action-packed' works of theatre. Enticing us with just a couple of the 'great tunes' from this deeply affecting opera, David Timson then begins setting the biographical and operatic scene. The enduring popularity of Tosca is quickly understood - its appeal generated largely from exactly the same elements which once caused academics to brand it a 'shabby little shocker'. The master of melody is at his finest, as David Timson illustrates with characteristic enthusiasm."
@@ -32,77 +32,77 @@ export const OPERA_DATA = [
   {
     id: 5,
     name: "Elektra",
-    composer: "Strauss",
+    composer: "Richard Strauss",
     language: "German",
     year: 1909
   },
   {
     id: 6,
     name: "La traviata",
-    composer: "Verdi",
+    composer: "Giuseppe Verdi",
     language: "Italian",
     year: 1853
   },
   {
     id: 7,
     name: "Rigoletto",
-    composer: "Verdi",
+    composer: "Giuseppe Verdi",
     language: "Italian",
     year: 1851
   },
   {
     id: 8,
     name: "Aida",
-    composer: "Verdi",
+    composer: "Giuseppe Verdi",
     language: "Italian",
     year: 1871
   },
   {
     id: 9,
     name: "Otello",
-    composer: "Verdi",
+    composer: "Giuseppe Verdi",
     language: "Italian",
     year: 1887
   },
   {
     id: 10,
     name: "Le nozze di Figaro",
-    composer: "Mozart",
+    composer: "Wolfgang Amadeus Mozart",
     language: "Italian",
     year: 1871
   },
   {
     id: 11,
     name: "Die Zauberflöte",
-    composer: "Mozart",
+    composer: "Wolfgang Amadeus Mozart",
     language: "German",
     year: 1871
   },
   {
     id: 12,
     name: "Tristan und Isolde",
-    composer: "Wagner",
+    composer: "Richard Wagner",
     language: "German",
     year: 1871
   },
   {
     id: 13,
     name: "Parsifal",
-    composer: "Wagner",
+    composer: "Richard Wagner",
     language: "German",
     year: 1871
   },
   {
     id: 14,
     name: "Lohengrin",
-    composer: "Wagner",
+    composer: "Richard Wagner",
     language: "German",
     year: 1871
   },
   {
     id: 15,
     name: "Madama Butterfly",
-    composer: "Puccini",
+    composer: "Giacomo Puccini",
     language: "Italian",
     year: 1904,
     summary: "Madama Butterfly runs the gamut of operatic emotions. The title heroine has a fragility wonderfully expressed in her entry music, deep sexual instincts as shown in the love duet, and immense tragic stature in her last hours. The fate of Madama Butterfly - married to an American sailor and then abandoned - engages our emotions, as it obviously did Puccini's to a degree equalled by few other opera heroines. The oriental setting and the exquisite orchestral and choral writings are treasured bonuses. This audiobook is the perfect introduction to Madama Butterfly. We learn about the background to Puccini's 'Japanese' opera, the social themes of honour and loyalty it involves, and how it progressed from a surprisingly unsuccessful premiere to one of the central and best-loved works of the international operatic repertory. As the story unfolds, we get to know the melodies which make this such a moving and compelling opera."
@@ -110,56 +110,56 @@ export const OPERA_DATA = [
   {
     id: 16,
     name: "Peter Grimes",
-    composer: "Britten",
+    composer: "Banjamin Britten",
     language: "English",
     year: 1939
   },
   {
     id: 17,
     name: "A Midsummer Night's Dream",
-    composer: "Britten",
+    composer: "Benjamin Britten",
     language: "English",
     year: 1943
   },
   {
     id: 18,
     name: "The Rape of Lucretia",
-    composer: "Britten",
+    composer: "Benjamin Britten",
     language: "English",
     year: 1840
   },
   {
     id: 19,
     name: "Der Rosenkavalier",
-    composer: "Strauss",
+    composer: "Richard Strauss",
     language: "German",
     year: 1911
   },
   {
     id: 20,
     name: "Ariadne auf Naxos",
-    composer: "Strauss",
+    composer: "Richard Strauss",
     language: "German",
     year: 1915
   },
   {
     id: 21,
     name: "War and Peace",
-    composer: "Prokofiev",
+    composer: "Sergei Prokofiev",
     language: "Russian",
     year: 1933
   },
   {
     id: 22,
     name: "The Love for Three Oranges",
-    composer: "Prokofiev",
+    composer: "Sergei Prokofiev",
     language: "Russian",
     year: 1841
   },
   {
     id: 23,
     name: "The Fiery Angel",
-    composer: "Prokofiev",
+    composer: "Sergei Prokofiev",
     language: "Russian",
     year: 1845
   }
