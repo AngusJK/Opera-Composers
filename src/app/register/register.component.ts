a7d9d856b79659d23d898b1f3663c086955efed3
Angus Kellett <anguskellett@hotmail.com>
2021-03-30 05:38:56 +0000

    imports from angular/form, creates getters, initializeForm method, and showInput method

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, Validators, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+
+  get name(): FormControl {
+    return this.registerForm.get('name') as FormControl;
+  }
+
+  get email(): FormControl {
+    return this.registerForm.get('email') as FormControl;
+  }
+  get password(): FormControl {
+    return this.registerForm.get('password') as FormControl;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    })
+  }
+
+  showInput() {
+    console.log(this.registerForm.value);
   }
 
 }
